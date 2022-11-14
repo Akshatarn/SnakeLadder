3a5a8679210e7b15c38b24b2f4578ca4f258655b
akshata_rn <akshatarn123@gamail.com>
2022-11-14 20:23:29 +0530

    Snake and Ladder for 2 Players

--- SnakeLadderProgram/Program.cs
+++ SnakeLadderProgram/Program.cs
@@ -7,53 +7,127 @@ using System.Threading.Tasks;
 namespace SnakeLadderProgram
 {
     internal class Program
-    {        
+    {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake And Ladder Simulation");
-            int position = 0;
-            Console.WriteLine("Initial(Start) position is:" + position);
-            int count = 0;
-            while (position >=0 && position < 100)
-            {
-                Random random = new Random();
-                int diceOutput = random.Next(1, 7);
-                count++;
-                Console.WriteLine("Dice Output is :" + diceOutput);
-                position = position + diceOutput;
-                Console.WriteLine("Position :" + position);
-                Random random2 = new Random();
-                Console.WriteLine("Checking for Options");
-                int option = random2.Next(1, 4);
-                switch (option)
+            int player1 = 0, player2 = 0,selectPlayer = 1;
+            
+            Console.WriteLine("Initial(Start) position1 is:" + player1);
+            int count1 = 0, count = 0;
+            
+                while (player2 < 100 && player1 < 100)
                 {
-                    case 1:
-                        Console.WriteLine("Ladder");
-                        position += diceOutput;
-                        if(position>=100)
+                    Random random = new Random();
+                    
+                    //count++;
+                    
+                    if (selectPlayer == 1)
                         {
-                            position = position - diceOutput;
+                            int diceOutput = random.Next(1, 7);
+                            // position = position + diceOutput;
+                            //Console.WriteLine("Position :" + position);
+                            if(player1 < 100 && player1 >= 0)
+                            {
+                                Console.WriteLine("Player1 DiceValue  is :" + diceOutput);
+                                Random random2 = new Random();
+                                Console.WriteLine("Checking for Options");
+                                int option = random2.Next(1, 4);
+                                switch (option)
+                                {
+                                    case 1:
+                                        Console.WriteLine("Ladder");
+                                        player1 += diceOutput;
+                                        selectPlayer = 1;
+                                        if (player1 > 100)
+                                        {
+                                            player1 -= diceOutput;
+                                        }
+                                        //Console.WriteLine("Position :" + position);
+                                        break;
+                                    case 2:
+                                        Console.WriteLine("Snake");
+                                        player1 -= diceOutput;
+                                        selectPlayer = 2;
+                                        if (player1 < 0)
+                                        {
+                                            player1 = 0;
+                                        }
+                                        //Console.WriteLine("Position :" + position);
+                                        break;
+                                    default:
+                                        Console.WriteLine("No Play");
+                                        //position = position;
+                                        //Console.WriteLine("Position :" + position);
+                                        break;
+                                }
+                                Console.WriteLine("Player1 Current position = " + player1);
+                                if(player1 < 0)
+                                {
+                                    player1 = 0;
+                                }                        
+                            }
+                            count++;
                         }
-                        Console.WriteLine("Position :" + position);
-                        break;
-                    case 2:
-                        Console.WriteLine("Snake");
-                        position -= diceOutput;
-                        if (position >0)
+                        else
                         {
-                            position = position + diceOutput;
-                        }
-                        Console.WriteLine("Position :" + position);
-                        break;
-                    default:
-                        Console.WriteLine("No Play");
-                        position = position;
-                        Console.WriteLine("Position :" + position);
-                        break;
+                            int diceOutput = random.Next(1, 7);
+                            if (player2 < 100 && player2 >= 0)
+                            {
+                                Console.WriteLine("Player2 DiceValue  is :" + diceOutput);
+                                Random random2 = new Random();
+                                Console.WriteLine("Checking for Options");
+                                int option = random2.Next(1, 4);
+                                switch (option)
+                                {
+                                    case 1:
+                                        Console.WriteLine("Ladder");
+                                        player2 += diceOutput;
+                                        selectPlayer = 2;
+                                        if (player2 > 100)
+                                        {
+                                            player2 -= diceOutput;
+                                        }
+                                        //Console.WriteLine("Position :" + position);
+                                        break;
+                                    case 2:
+                                        Console.WriteLine("Snake");
+                                        player2 -= diceOutput;
+                                        selectPlayer = 1;
+                                        if (player2 < 0)
+                                        {
+                                            player2 = 0;
+                                        }
+                                        //Console.WriteLine("Position :" + position);
+                                        break;
+                                    default:
+                                        Console.WriteLine("No Play");
+                                        //position = position;
+                                        //Console.WriteLine("Position :" + position);
+                                        break;
+                                }
+                                Console.WriteLine("Player2 current postion = "+ player2);
+                            }
+                                if (player2 < 0)
+                                {
+                                    player2 = 0;
+                                }
+                            count1++;
                 }
-            } 
-            Console.WriteLine("No of times dice thrown is : "+ count);
-           Console.ReadLine(); 
+            }
+            Console.WriteLine();
+            Console.WriteLine("Player1 dice rolled count =" + count);
+            Console.WriteLine("Player2 dice rolled count =" + count1);
+            Console.WriteLine("Total dice count =" + (count+count1));
+            if(player1 == 100)
+            {
+                Console.WriteLine("Player1 wins");
+            }
+            else
+            {
+                Console.WriteLine("Player2 Wins");
+            }
+
         }
     }
 }
