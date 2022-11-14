b1173bbda52598de7801596b3dc108c959e1e910
akshata_rn <akshatarn123@gamail.com>
2022-11-14 12:46:02 +0530

    Rolling dice to get number from 1 to 6

--- SnakeLadderProgram/Program.cs
+++ SnakeLadderProgram/Program.cs
@@ -13,7 +13,10 @@ namespace SnakeLadderProgram
         {
             Console.WriteLine("Welcome to Snake And Ladder Simulation");
             int position = 0;
-            Console.WriteLine("Initial(Start) position is:" + position);  
+            Console.WriteLine("Initial(Start) position is:" + position); 
+            Random random = new Random();
+            int diceOutput = random.Next(1, 7);
+            Console.WriteLine("Dice Output is :" + diceOutput);
             Console.ReadLine();
         }
     }
