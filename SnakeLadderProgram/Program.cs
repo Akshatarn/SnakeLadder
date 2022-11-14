562a07f24566b30ef3624bc1c0d5b6d5b8ff982c
akshata_rn <akshatarn123@gamail.com>
2022-11-14 15:09:02 +0530

    Repeating while player reaches position 100

--- SnakeLadderProgram/Program.cs
+++ SnakeLadderProgram/Program.cs
@@ -7,40 +7,43 @@ using System.Threading.Tasks;
 namespace SnakeLadderProgram
 {
     internal class Program
-    {
-        
+    {        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake And Ladder Simulation");
             int position = 0;
-            Console.WriteLine("Initial(Start) position is:" + position); 
-            Random random = new Random();
-            int diceOutput = random.Next(1, 7);
-            Console.WriteLine("Dice Output is :" + diceOutput);
-            position = diceOutput;
-            Console.WriteLine("Position :" + position);
-            Random random2 = new Random();
-            Console.WriteLine("Checking for Options");
-            int option = random2.Next(1, 4);
-            switch(option)
+            Console.WriteLine("Initial(Start) position is:" + position);
+           
+            while (position >=0 && position < 100)
             {
-                case 1:
-                    Console.WriteLine("No Play");
-                    position = position;
-                    Console.WriteLine("Position :" + position);
-                    break;
-                case 2:
-                    Console.WriteLine("Ladder");
-                    position += diceOutput;
-                    Console.WriteLine("Position :" + position);
-                    break;
-                case 3:
-                    Console.WriteLine("Snake");
-                    position -= diceOutput;
-                    Console.WriteLine("Position :" + position);
-                    break;  
-            }
-            Console.ReadLine();
+                Random random = new Random();
+                int diceOutput = random.Next(1, 7);
+                Console.WriteLine("Dice Output is :" + diceOutput);
+                position = position + diceOutput;
+                Console.WriteLine("Position :" + position);
+                Random random2 = new Random();
+                Console.WriteLine("Checking for Options");
+                int option = random2.Next(1, 4);
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("No Play");
+                        position = position;
+                        Console.WriteLine("Position :" + position);
+                        break;
+                    case 2:
+                        Console.WriteLine("Ladder");
+                        position += diceOutput;
+                        Console.WriteLine("Position :" + position);
+                        break;
+                    case 3:
+                        Console.WriteLine("Snake");
+                        position -= diceOutput;
+                        Console.WriteLine("Position :" + position);
+                        break;
+                }
+            } 
+           Console.ReadLine(); 
         }
     }
 }
