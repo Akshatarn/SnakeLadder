aafcc4afacbf87927fc54881871e56c35924986b
akshata_rn <akshatarn123@gamail.com>
2022-11-14 12:36:13 +0530

    Snake and Ladder Simulation

---
+++ SnakeLadderProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProgram
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder Simulation");
+            Console.ReadLine();
+        }
+    }
+}
